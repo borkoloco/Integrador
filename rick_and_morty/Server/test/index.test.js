19d64d94bdbf4c40cf64437b4b1723038f3aa945
Santiago Bosca Chillida <borkoloco@MacBook-Air-de-Santiago.local>
2023-09-11 15:09:12 -0300

    updated

--- rick_and_morty/Server/test/index.test.js
+++ rick_and_morty/Server/test/index.test.js
@@ -2,62 +2,100 @@ const app = require("../src/app");
 const session = require("supertest");
 const agent = session(app);
 
+const char = {
+  id: 1,
+  name: "Rick Sanchez",
+  status: "Alive",
+  species: "Human",
+  gender: "Male",
+  origin: {
+    name: "Earth (C-137)",
+    url: "https://rickandmortyapi.com/api/location/1",
+  },
+  image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+};
+const char2 = {
+  id: 2,
+  name: "Morty Smith",
+  status: "Alive",
+  species: "Human",
+  gender: "Male",
+  origin: {
+    name: "unknown",
+    url: "",
+  },
+  image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+};
+
+//agent es una instancia de app que se aplica sobre supertest
+
 describe("Test de RUTAS", () => {
   describe("GET /rickandmorty/character/:id", () => {
     it("Responde con status: 200", async () => {
       await agent.get("/rickandmorty/character/1").expect(200);
     });
     it('Responde un objeto con las propiedades: "id", "name", "species", "gender", "status", "origin" e "image"', async () => {
-      const res = await agent.get("/rickandmorty/character/1");
-      expect(res).toHaveProperty("id");
-      expect(res).toHaveProperty("name");
-      expect(res).toHaveProperty("species");
-      expect(res).toHaveProperty("gender");
-      expect(res).toHaveProperty("status");
-      expect(res).toHaveProperty("origin");
-      expect(res).toHaveProperty("image");
+      const resp = await agent.get("/rickandmorty/character/1");
+      expect(resp.body).toHaveProperty("id");
+      expect(resp.body).toHaveProperty("name");
+      expect(resp.body).toHaveProperty("species");
+      expect(resp.body).toHaveProperty("gender");
+      expect(resp.body).toHaveProperty("status");
+      expect(resp.body).toHaveProperty("origin");
+      expect(resp.body).toHaveProperty("image");
     });
-    it("Si hay un error responde con status: 500", async () => {
-      const res = await request(app).get("/:id");
-      expect(res.statusCode).toBe(500);
+    //  con status 500 no funciona!!!!
+    it("Si hay un error responde con status: 500/404", async () => {
+      const response = await agent.get("/rickandmorty/character/1000");
+      expect(response.statusCode).toBe(404);
     });
   });
+
   describe("GET /rickandmorty/login", () => {
-    it("Debe retornar un objeto si la informacion es correcta", async () => {
-      const res = await request(app).get(
-        "/rickandmorty/login?password=mondiola123"
+    let email = "mondiola@gmail.com";
+    let password = "mondiola123";
+    it("Debe retornar un objeto en TRUE si la informacion es correcta", async () => {
+      const res = await agent.get(
+        "/rickandmorty/login?email=" + email + "&password=" + password
       );
-      expect(res.body).toBe({
+      expect(res.body).toEqual({
         access: true,
       });
     });
-    it("La propiedad debe ser false si la informacion es incorrecta", async () => {
-      const res = await request(app).get(
-        "/rickandmorty/login?password!=mondiola123"
+    it("La propiedad debe ser FALSE si la informacion es incorrecta", async () => {
+      const res = await agent.get(
+        "/rickandmorty/login?email=" + email + "&password="
       );
-      expect(res.body).toBe({
+      expect(res.body).toEqual({
         access: false,
       });
     });
   });
+
+  //chequear matcher toContainEqual
+  //para este test habria que copiar dos personajes para verificar(ver arriba)
   describe("POST /rickandmorty/fav", () => {
     it("La respuesta debe retornar un arreglo", async () => {
-      const res = await request(app).post("/rickandmorty/fav");
-      expect(res.body).toBe();
-    });
-    it("Otro envio debe incluir el objeto previo en el arrego", async () => {
-      const res = await request(app).post("/rickandmorty/fav");
-      expect(res.body).toBe();
+      let data = await agent.post("/rickandmorty/fav").send(char);
+      expect(Array.isArray(data.body)).toBe(true);
+      expect(data.body.length).toBe(1);
+      expect(data.body).toContainEqual(char);
+      data = await agent.post("/rickandmorty/fav").send(char2);
+      expect(data.body.length).toBe(2);
+      expect(data.body).toContainEqual(char);
     });
   });
+
+  //ya tengo un array con dos personajes con id, puedo usar ese array para testear
   describe("DELETE /rickandmorty/fav/:id", () => {
     it("Si no existe el id debe retornar el mismo arreglo", async () => {
-      const res = await request(app).delete("/rickandmorty/fav/:id");
-      expect(res.body).toBe();
-    });
-    it("Si el id es valido el personaje debe ser eliminado correctamente", async () => {
-      const res = await request(app).delete("/rickandmorty/fav/:id");
-      expect(res.body).toBe();
+      let data = await agent.delete("/rickandmorty/fav/5");
+      [char, char2].forEach((ele) => {
+        expect(data.body).toContainEqual(ele);
+      });
+      data = await agent.delete("/rickandmorty/fav/1");
+      expect(data.body).toContainEqual(char2);
+      expect(data.body).not.toContainEqual(char);
     });
   });
 });
